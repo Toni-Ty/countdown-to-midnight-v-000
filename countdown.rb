77e861ced331253ebfa02e5fc3760b2ccea2f71f
Toni-Ty <antonique2479@yahoo.com>
2018-12-17 06:10:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
-
-def countdown
+def countdown (number)
+while number > 0 #while countdown is less than 10, execute code in loop.
+  puts "#{number} SECOND(S)!"
+  number -= 1 #counter is counting down from integer of 10
+  end
+ "HAPPY NEW YEAR!"
 end
+
+# puts "HAPPY NEW YEAR!" #return phrase after breaking out of loop
+
+# def countdown_with_sleep
+def countdown_with_sleep (number)
+  
+ end
+end
+  
