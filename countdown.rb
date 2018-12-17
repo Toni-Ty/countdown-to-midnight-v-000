b3f623e56bf4cba92d40e740ab426bdca42773de
Toni-Ty <antonique2479@yahoo.com>
2018-12-17 06:17:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,9 +10,17 @@ end
 # puts "HAPPY NEW YEAR!" #return phrase after breaking out of loop
 
 # def countdown_with_sleep
-def countdown_with_sleep (number)
-  while number == 5
-    number -= 1
-
-  end
-end
+# def countdown_with_sleep (number)
+#   while number == 5
+#     number -= 1
+# 
+#   end
+# end
+def countdown_with_sleep(num) 
+  one_sec = sleep 1 
+  while num > 0
+    puts "#{num}" 
+    num -= one_sec
+  end 
+  return "Goodnight"
+end 
